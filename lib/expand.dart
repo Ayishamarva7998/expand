48d93de15382c458495d7c772947f9ee4e473f4f
Ayishamarva7998 <ayshaaysha7998@gmail.com>
2023-09-13 00:43:31 +0530

    first commit

--- lib/expand.dart
+++ lib/expand.dart
@@ -1,51 +1,40 @@
-
 import 'package:flutter/material.dart';
 
-class ex extends StatelessWidget {
-  const ex({super.key});
+class exp extends StatelessWidget {
+  const exp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar:AppBar(
-        title: Text("Expanded"),
-        backgroundColor:Colors.green  ,
-      ),
-      body: Row(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Expanded(
-            flex: 3,
-            child: Container(
-              height: 50,
-              width: 70,
-              color: Colors.red,
-            ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(
+    return SafeArea(
+      child: Scaffold(
+        
+        appBar: AppBar(
+          title: Text('MainAxisSize.max'),
+          backgroundColor: Colors.green,
+          
+        ),
+        body: Container(
+          margin: EdgeInsets.only(top: 20),
+          height: 200,
+          width: 50,
+          color: Colors.yellow,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
               
-              height: 50,
-              width: 70,
-              color: Colors.green,
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              height: 50,
-              width: 70,
-              color: Colors.blue,
-            ),
-          ),
+              Icon(Icons.star),
+              Icon(Icons.star),
+              Icon(Icons.star)
+              
+            ],
 
-        ],
-      ),
-
+            
+            
+          ),
+        ),
+      )
     );
-      
-    
+        
+        
   }
 }
-      
--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "expand",
-      home: ex(),
+      home: exp(),
     );
   }
 }
